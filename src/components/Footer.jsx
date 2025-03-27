4ecd41770d559357d260eb6254a83976cb968140
Nishaf <shahmurrawat@gmail.com>
2025-03-27 17:32:08 +0500

    Adding frontend on git

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+      {/* Left section */}
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Schedule your appointment with our experienced doctors effortlessly.
+We provide quality healthcare with a patient-first approach.
+Your health is our priority—trust us for expert medical care.
+Contact us today to book your consultation!</p>
+        </div>
+        {/* center section */}
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        {/* Right section */}
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>0327-7054143</li>
+            <li>shahmurrawat@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
